8273cdb4af5b071771016a800965a359e26b784b
Jacob Keil <jacobkeil13@gmail.com>
2021-05-25 20:35:00 -0400

    asd

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
+const team1 = require("../views/data/team1.json");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
     res.render("map.ejs", {
         mapkey: process.env.MAPBOX_KEY,
-        style: process.env.MAPBOX_STYLE
+        style: process.env.MAPBOX_STYLE,
+        teamone: team1
     });
 });
 
--- src/views/js/map.js
+++ src/views/js/map.js
@@ -1,73 +1,325 @@
+const team1 = [
+    {
+      "Name": "Avant Garde",
+      "Type": "Charter",
+      "Coordinates": [28.07770357390398, -82.5770077133423]
+    },
+    {
+      "Name": "Henderson Hammock",
+      "Type": "Charter",
+      "Coordinates": [28.043809580918783, -82.55102661402559]
+    },
+    {
+      "Name": "Hillsborough Math Science",
+      "Type": "Charter",
+      "Coordinates": [28.030191953910794, -82.59674025672525]
+    },
+    {
+      "Name": "Horizon",
+      "Type": "Charter",
+      "Coordinates": [27.99811516622201, -82.56213886594166]
+    },
+    {
+      "Name": "Learning Gate",
+      "Type": "Charter",
+      "Coordinates": [28.103761661873296, -82.45030854411765]
+    },
+    {
+      "Name": "Legacy Prep",
+      "Type": "Charter",
+      "Coordinates": [28.04026231679742, -82.45759448557814]
+    },
+    {
+      "Name": "Lutz Prep",
+      "Type": "Charter",
+      "Coordinates": [28.132548731687752, -82.46261910969288]
+    },
+    {
+      "Name": "Plato Academy Tampa",
+      "Type": "Charter",
+      "Coordinates": [28.079141246688014, -82.57232067367123]
+    },
+    {
+      "Name": "SLAM",
+      "Type": "Charter",
+      "Coordinates": [28.068476604685337, -82.5708728487809]
+    },
+    {
+      "Name": "Sunlake Academy",
+      "Type": "Charter",
+      "Coordinates": [28.143163761848662, -82.49462745919374]
+    },
+    {
+      "Name": "Trinity School",
+      "Type": "Charter",
+      "Coordinates": [27.98771757555424, -82.48317006775433]
+    },
+    {
+      "Name": "Walton",
+      "Type": "Charter",
+      "Coordinates": [27.989401299392114, -82.45924548942764]
+    },
+    {
+      "Name": "Alexander",
+      "Type": "Elementary",
+      "Coordinates": [27.998087052010014, -82.51418766635796]
+    },
+    {
+      "Name": "Bay Crest",
+      "Type": "Elementary",
+      "Coordinates": [27.99253231330779, -82.57469775915253]
+    },
+    {
+      "Name": "Bellamy ",
+      "Type": "Elementary",
+      "Coordinates": [28.035890373135416, -82.56759997268793]
+    },
+    {
+      "Name": "Bryant",
+      "Type": "Elementary",
+      "Coordinates": [28.072504593042066, -82.63206948119657]
+    },
+    {
+      "Name": "Cannella",
+      "Type": "Elementary",
+      "Coordinates": [28.045645126047386, -82.53277800584948]
+    },
+    {
+      "Name": "Carrolwood",
+      "Type": "Elementary",
+      "Coordinates": [28.052568019496583, -82.49943070727922]
+    },
+    {
+      "Name": "Citrus Park",
+      "Type": "Elementary",
+      "Coordinates": [28.07807589481273, -82.57360447647561]
+    },
+    {
+      "Name": "Claywell",
+      "Type": "Elementary",
+      "Coordinates": [28.099406465902305, -82.5225374039227]
+    },
+    {
+      "Name": "Crestwood",
+      "Type": "Elementary",
+      "Coordinates": [28.019218097626847, -82.51885421668898]
+    },
+    {
+      "Name": "Davis",
+      "Type": "Elementary",
+      "Coordinates": [28.016360567988286, -82.60162327268812]
+    },
+    {
+      "Name": "Deer Park",
+      "Type": "Elementary",
+      "Coordinates": [28.067639278143005, -82.61571854200278]
+    },
+    {
+      "Name": "Dickenson",
+      "Type": "Elementary",
+      "Coordinates": [27.98853822652344, -82.56626340616923]
+    },
+    {
+      "Name": "Egypt Lake",
+      "Type": "Elementary",
+      "Coordinates": [28.007612510341588, -82.49843651196885]
+    },
+    {
+      "Name": "Essrig",
+      "Type": "Elementary",
+      "Coordinates": [28.06832750285965, -82.54123363581868]
+    },
+    {
+      "Name": "Forest Hills",
+      "Type": "Elementary",
+      "Coordinates": [28.04203217475472, -82.46423290152347]
+    },
+    {
+      "Name": "Hammond ",
+      "Type": "Elementary",
+      "Coordinates": [28.11378694235735, -82.57416400152277]
+    },
+    {
+      "Name": "Lake Magdalene",
+      "Type": "Elementary",
+      "Coordinates": [28.05718894671791, -82.4776340726877]
+    },
+    {
+      "Name": "Lowry",
+      "Type": "Elementary",
+      "Coordinates": [28.03827793450622, -82.61220097268789]
+    },
+    {
+      "Name": "Lutz ",
+      "Type": "K-8",
+      "Coordinates": [28.147810362667705, -82.45935597268691]
+    },
+    {
+      "Name": "MacFarlane Park",
+      "Type": "Elementary",
+      "Coordinates": [27.9580667007821, -82.49283852278961]
+    },
+    {
+      "Name": "Maniscalco",
+      "Type": "K-8",
+      "Coordinates": [28.12596981776297, -82.44784118580715]
+    },
+    {
+      "Name": "McKitrick ",
+      "Type": "Elementary",
+      "Coordinates": [28.156402889006003, -82.53186865113169]
+    },
+    {
+      "Name": "Mendenhall",
+      "Type": "Elementary",
+      "Coordinates": [27.99403021943954, -82.47967003710609]
+    },
+    {
+      "Name": "Miles",
+      "Type": "Elementary",
+      "Coordinates": [28.058955450525875, -82.45665966905513]
+    },
+    {
+      "Name": "Morgan Woods",
+      "Type": "Elementary",
+      "Coordinates": [28.012932954532477, -82.5595090303594]
+    },
+    {
+      "Name": "Northwest ",
+      "Type": "Elementary",
+      "Coordinates": [28.105449840638464, -82.54868071277613]
+    },
+    {
+      "Name": "Oak Grove",
+      "Type": "Elementary",
+      "Coordinates": [28.004548002655188, -82.48229968253034]
+    },
+    {
+      "Name": "Schwartzkopf",
+      "Type": "Elementary",
+      "Coordinates": [28.137960696870504, -82.50538951284848]
+    },
+    {
+      "Name": "Tampa Bay Blvd",
+      "Type": "Elementary",
+      "Coordinates": [27.975141646577722, -82.49389326594189]
+    },
+    {
+      "Name": "Town & Country",
+      "Type": "Elementary",
+      "Coordinates": [28.000890373292744, -82.56231630827047]
+    },
+    {
+      "Name": "Twin Lakes",
+      "Type": "Elementary",
+      "Coordinates": [28.029139726889124, -82.48803121335234]
+    },
+    {
+      "Name": "West Tampa",
+      "Type": "Elementary",
+      "Coordinates": [27.96128964464151, -82.48757729499117]
+    },
+    {
+      "Name": "Westchase",
+      "Type": "Elementary",
+      "Coordinates": [28.04495886758262, -82.59258794542176]
+    },
+    {
+      "Name": "Woodbridge",
+      "Type": "Elementary",
+      "Coordinates": [28.014902809001338, -82.57558466991685]
+    },
+    {
+      "Name": "Roland Park",
+      "Type": "K8",
+      "Coordinates": [27.956151982795024, -82.51892955828221]
+    },
+    {
+      "Name": "Adams",
+      "Type": "Middle",
+      "Coordinates": [28.041845451976595, -82.46723970458993]
+    },
+    {
+      "Name": "Ben Hill",
+      "Type": "Middle",
+      "Coordinates": [28.08405233311216, -82.53591229999029]
+    },
+    {
+      "Name": "Buchanan",
+      "Type": "Middle",
+      "Coordinates": [28.089454182638278, -82.46884490152303]
+    },
+    {
+      "Name": "Davidsen",
+      "Type": "Middle",
+      "Coordinates": [28.053841015761588, -82.59633968634158]
+    },
+    {
+      "Name": "Farnell",
+      "Type": "Middle",
+      "Coordinates": [28.073325916099286, -82.63180495919438]
+    },
+    {
+      "Name": "Martinez",
+      "Type": "Middle",
+      "Coordinates": [28.16125801234734, -82.53304093405649]
+    },
+    {
+      "Name": "Pierce",
+      "Type": "Middle",
+      "Coordinates": [27.998313382675466, -82.51825623035951]
+    },
+    {
+      "Name": "Sgt. Smith",
+      "Type": "Middle",
+      "Coordinates": [28.076218478563757, -82.57302368526781]
+    },
+    {
+      "Name": "Walker",
+      "Type": "Middle",
+      "Coordinates": [28.11276421956104, -82.57634435734478]
+    },
+    {
+      "Name": "Webb",
+      "Type": "Middle",
+      "Coordinates": [28.002809182673506, -82.56087185919505]
+    }
+   ]
+
+var team1geo = {
+    type: "FeatureCollection",
+    features: [],
+};
+
 map.addControl(
     new mapboxgl.FullscreenControl({
       container: document.querySelector("body"),
     })
   );
 
-  var geojson = {
-    type: "FeatureCollection",
-    features: [
-      {
+for (let i = 0; i < team1.length; i++) {
+
+    var feat = {
         type: "Feature",
         geometry: {
-          type: "Point",
-          coordinates: [-82.5595, 28.013],
+            type: "Point",
+            coordinates: [team1[i].Coordinates[1], team1[i].Coordinates[0]],
         },
         properties: {
-          title: "Morgan Woods Elementary School",
-          description: "Morgan Woods",
+            title: team1[i].Name,
+            description: team1[i].Type,
         },
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-82.562, 28.001],
-        },
-        properties: {
-          title: "Town and Country Elementary School",
-          description: "Town and Country",
-        },
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-82.566, 27.988],
-        },
-        properties: {
-          title: "Bay Crest Elementary School",
-          description: "Bay Crest",
-        },
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-82.573, 27.992],
-        },
-        properties: {
-          title: "Dickenson Elementary School",
-          description: "Dickenson",
-        },
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-82.576, 28.015],
-        },
-        properties: {
-          title: "Woodbridge Elementary School",
-          description: "8301 Woodbridge Blvd, Tampa, FL 33615",
-        },
-      }
-    ],
-  };
+    }
+
+    team1geo.features.push(feat);
+}
+
+console.log(team1geo.features[0].properties.title);
+
   // add markers to map
-  geojson.features.forEach(function (marker) {
+  team1geo.features.forEach(function (marker) {
     var el = document.createElement("div");
-    el.className = "marker fas fa-map-marker-alt fa-2x";
+    el.className = "marker fas fa-map-marker-alt fa-2x yellow";
 
     // make a marker for each feature and add to the map
     new mapboxgl.Marker(el, {
@@ -76,7 +328,7 @@ map.addControl(
       .setLngLat(marker.geometry.coordinates)
       .setPopup(
         new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML("<h3>" + marker.properties.title + '</h3><i class="fas fa-directions"></i><a href="http://www.google.com/maps/place/' + marker.geometry.coordinates[1] + "," + marker.geometry.coordinates[0] + '">Directions<h4>')
+          .setHTML("<h3>" + marker.properties.title + " " + marker.properties.description + ' School</h3><i class="fas fa-directions"></i><a href="http://www.google.com/maps/place/' + marker.geometry.coordinates[1] + "," + marker.geometry.coordinates[0] + '">Directions<h4>')
       )
       .addTo(map);
   });
